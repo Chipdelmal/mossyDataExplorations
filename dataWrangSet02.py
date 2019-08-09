c1dd0295a5a2365bf6f4047d89c35d614c0c7599
chipdelmal <chipdelmal@gmail.com>
2019-08-09 15:45:45 -0700

    Merge export done.

--- dataWrangSet02.py
+++ dataWrangSet02.py
@@ -12,8 +12,8 @@ import glob
 # ############################################################################
 # Setup paths and categories (cats)
 # ############################################################################
-PATH = "/Users/sanchez.hmsc/Documents/GitHub/mozzieDataExplorations/"
-FILEPATHS = glob.glob(PATH + "data/VectorBase*")
+PATH = "/Volumes/marshallShare/FieldData/"
+FILEPATHS = glob.glob(PATH + "VectorBase*")
 catsOfInterest = [
     'collection_ID', 'sample_ID', 'trap_id',
     'collection_date_start', 'collection_date_end',
@@ -27,30 +27,34 @@ catsOfInterest = [
 catsDates = ['collection_date_start', 'collection_date_end']
 
 # ############################################################################
-# Load raw dataset
+# Load raw dataset into data full (daf) and data of interest (doi)
 # ############################################################################
 dataframes = []
 for file in FILEPATHS:
     dataTemp = pd.read_csv(file, parse_dates=catsDates)
     dataframes.append(dataTemp)
-doi = pd.concat(dataframes, axis=0, ignore_index=True)
+daf = pd.concat(dataframes, axis=0, ignore_index=True)
 doi[catsOfInterest]
+
+# ############################################################################
+# Export merged
+# ############################################################################
+daf.to_csv(path_or_buf=(PATH + "Clean/VectorBase_mergedDAF.csv"))
+doi.to_csv(path_or_buf=(PATH + "Clean/VectorBase_mergedDOI.csv"))
+
 
 # ############################################################################
 # Do some basic exploration on types and entries
 # ############################################################################
-mem = data.memory_usage(deep=True)
-cols = list(data.columns)
-shp = data.shape
-cols
+mem = daf.memory_usage(deep=True)
+cols = list(daf.columns)
+shp = daf.shape
+mem
 
-
-
-
-# ############################################################################
+ ############################################################################
 # Print variables sets
 # ############################################################################
 cats = {}
 for i in cols:
-    cats.update({i: set(data[i])})
-    print("{}:{}".format(i, set(data[i])))
+    cats.update({i: set(daf[i])})
+    print("{}:{}".format(i, set(daf[i])))
