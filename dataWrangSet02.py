0dbd4f58c54007af848eb63c276c17792cebdf65
chipdelmal <chipdelmal@gmail.com>
2019-08-09 15:36:46 -0700

    VectorBase datasets are now stored in a single dataframe

--- dataWrangSet02.py
+++ dataWrangSet02.py
@@ -6,23 +6,35 @@
 #   https://zenodo.org/search?page=1&size=20&q=Manatee%20County%20Mosquito%20Control%20District
 # ############################################################################
 import pandas as pd
+import glob
+
 
 # ############################################################################
-# Set paths and filenames
+# Setup paths and categories (cats)
 # ############################################################################
-year = 2016
-(PATH, FILENAME) = (
-    "/Users/sanchez.hmsc/Documents/GitHub/mozzieDataExplorations/",
-    "VectorBase-" +
-        str(year) +
-        "-Manatee_County_Mosquito_Control_District_Florida_USA.csv"
-)
+PATH = "/Users/sanchez.hmsc/Documents/GitHub/mozzieDataExplorations/"
+FILEPATHS = glob.glob(PATH + "data/VectorBase*")
+catsOfInterest = [
+    'collection_ID', 'sample_ID', 'trap_id',
+    'collection_date_start', 'collection_date_end',
+    'GPS_lat', 'GPS_lon', 'location_ADM1',
+    'trap_type', 'attractant',
+    'trap_number', 'trap_duration',
+    'species', 'identification_method',
+    'developmental_stage', 'sex',
+    'sample_count'
+]
+catsDates = ['collection_date_start', 'collection_date_end']
 
 # ############################################################################
 # Load raw dataset
 # ############################################################################
-data = pd.read_csv('data/' + FILENAME)
-list(data.columns)
+dataframes = []
+for file in FILEPATHS:
+    dataTemp = pd.read_csv(file, parse_dates=catsDates)
+    dataframes.append(dataTemp)
+doi = pd.concat(dataframes, axis=0, ignore_index=True)
+doi[catsOfInterest]
 
 # ############################################################################
 # Do some basic exploration on types and entries
@@ -30,7 +42,9 @@ list(data.columns)
 mem = data.memory_usage(deep=True)
 cols = list(data.columns)
 shp = data.shape
-print(cols)
+cols
+
+
 
 
 # ############################################################################
